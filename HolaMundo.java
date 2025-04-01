712a889efdb10afccf69f6e9c8a400064e10449d
rxdry <71684617+rxdry@users.noreply.github.com>
2025-03-31 22:09:58 -0300

    HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,7 +4,7 @@ package com.mycompany.holamundo;
 public class HolaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        System.out.println("Como estan???");
+        System.out.println("oda chavales");
+        System.out.println("como estais???");
     }
 }
