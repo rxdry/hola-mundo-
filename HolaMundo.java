209bca2af16468a91a5311945bf9eb678acbdc20
rxdry <71684617+rxdry@users.noreply.github.com>
2025-03-31 22:09:20 -0300

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+//esto es un hola mundo
+package com.mycompany.holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Como estan???");
+    }
+}
